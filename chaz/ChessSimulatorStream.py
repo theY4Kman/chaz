d6e02c65d287d41748620f3ef81665c570e37778
Bryan Tremblay <bryanandhallie@yahoo.com>
2014-01-12 05:12:16 +0900

    Minor Changes

---
+++ chaz/ChessSimulatorStream.py
@@ -0,0 +1,5 @@
+class ChessSimulatorStream:
+	def __init__(self):
+		pass
+	def sendCommand(self, command):
+		raise NotImplementedError()
--- chaz/ToImplement/player.py
+++ chaz/ToImplement/player.py
@@ -1,6 +1,5 @@
 class BasePlayer(object):
 	def setup(self, color, state):
 		pass
-
 	def next_move(self, state):
 		raise NotImplementedError()
